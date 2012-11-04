163be8c960da8cab08bb0025dd9ee5a46b291e57
Yaniv Kalsky <yanivka@gmail.com>
2012-11-05 01:41:28 +0200

    Initial Commit

---
+++ SugarSyncAPI Example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SugarSyncAPI Example
+//
+//  Created by Yaniv Kalsky on 05/11/12.
+//  Copyright (c) 2012 Yaniv Kalsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
